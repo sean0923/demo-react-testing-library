db0aabc69540bf4d0a090ac5ab52137094af3148
Sean Hong <iamseanhong@gmail.com>
2020-07-07 00:01:41 -0600

    Clean up async stuff test solution"

--- src/components/async-stuff/async-stuff.test-solution.tsx
+++ src/components/async-stuff/async-stuff.test-solution.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import { AsyncStuff } from './async-stuff.component';
 
